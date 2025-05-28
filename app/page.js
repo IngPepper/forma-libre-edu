a9173940217a8374989cb3640fad63bcd7756891
Charlie <lucalehemx@gmail.com>
2025-05-28 00:33:14 -0600

    Minor component path set to root

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 
 import styles from "./page.module.css";
-import MainNav from './components/MainNav';
-import MainFooter from './components/MainFooter';
-import ContentSection from "./components/ContentSection";
+import MainNav from '@/components/MainNav';
+import MainFooter from '@/components/MainFooter';
+import ContentSection from "@/components/ContentSection";
 
 export default function Home() {
   return (
